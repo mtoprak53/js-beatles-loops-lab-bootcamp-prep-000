db80d6cd4b26458c436a6b210083e4e7f6607f16
Mehmet Toprak <mtoprak53@gmail.com>
2018-07-12 21:34:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,3 +15,10 @@ function johnLennonFacts(facts) {
   return facts
 }
 
+function iLoveTheBeatles(number){
+  var newArray = []
+  do {
+    newArray.push("I love the Beatles!")
+    number++
+  } while (number < 15)
+}
