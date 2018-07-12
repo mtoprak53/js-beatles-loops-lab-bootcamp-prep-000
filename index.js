87845c92f58700bb334d9728bd0e98e72184395a
Mehmet Toprak <mtoprak53@gmail.com>
2018-07-12 21:22:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,11 +2,8 @@
 
 function theBeatlesPlay(musicians, instruments) {
   var newArray = [];
-  var mus, ins
   for (var i = 0; i < musicians.length; i++) {
-    mus = musicians[i]
-    ins = instruments[i]
-    newArray[i] = "${mus} plays ${ins}"
+    newArray[i] = musicians[i] + " plays " + instruments[i]
   }
   return newArray
 }
